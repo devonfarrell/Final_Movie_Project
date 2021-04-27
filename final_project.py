461c5ac166fa593b5aec8d952ad36b1cd7a623e8
devonfarrell <70960248+devonfarrell@users.noreply.github.com>
2021-04-27 15:59:31 -0400

    Adding finished file

--- final_project.py
+++ final_project.py
@@ -101,7 +101,7 @@ base_url_other = 'https://api.tmdb.org/3/discover/movie/?api_key='+key
 
 #Database Creation 
 
-DBNAME = 'movie_info.sqlite'
+DBNAME = 'movie_information.sqlite'
 
 def init_db():
     conn = sqlite3.connect(DBNAME)
@@ -124,17 +124,10 @@ def init_db():
         DROP TABLE IF EXISTS 'Additional_Movies';
 
     '''
+    drop_additional_ratings = '''
 
-    create_movie = '''
-        
-        CREATE TABLE 'Movies' (
-                'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
-                'Title' TEXT NOT NULL,
-                'Ranking' INTERGER,
-                'Year' INTEGER,
-                'AverageRating' REAL NOT NULL,
-                'ContentRating' TEXT NOT NULL
-        );
+        DROP TABLE IF EXISTS 'Additional_Movies_Ratings';
+
     '''
 
     create_stars = '''
@@ -156,10 +149,37 @@ def init_db():
         );
     '''
 
+    create_movie = '''
+        
+        CREATE TABLE 'Movies' (
+                'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+                'Title' TEXT NOT NULL,
+                'Ranking' INTEGER NOT NULL,
+                'Year' INTEGER NOT NULL,
+                'AverageRating' REAL NOT NULL,
+                'ContentRating' TEXT NOT NULL
+        );
+    '''
+
+    create_additional_ratings = '''
+
+    CREATE TABLE 'Additional_Movies_Ratings' (
+        'Id' INTEGER PRIMARY KEY AUTOINCREMENT, 
+        'movie_id' TEXT NOT NULL,
+        'total_votes' INTEGER NOT NULL, 
+        'mean_votes' INTEGER NOT NULL,
+        'median_votes' INTEGER NOT NULL,
+        'male_votes' INTEGER NOT NULL,
+        'female_votes' INTEGER NOT NULL
+    );
+
+    '''
+
     create_additional_movies = '''
 
         CREATE TABLE 'Additional_Movies' (
-            'Id' INTEGER PRIMARY KEY AUTOINCREMENT,
+            'Id' INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+            'movie_id' TEXT NOT NULL,
             'Title' TEXT NOT NULL,
             'Years' INTEGER NOT NULL, 
             'data_published' REAl NOT NULL, 
@@ -179,14 +199,19 @@ def init_db():
         );
 
     '''
-    cur.execute(drop_movie)
-    cur.execute(create_movie)
+ 
+
     cur.execute(drop_star)
     cur.execute(create_stars)
     cur.execute(drop_director)
     cur.execute(create_director)
+    cur.execute(drop_movie)
+    cur.execute(create_movie)
     cur.execute(drop_csv)
     cur.execute(create_additional_movies)
+    cur.execute(drop_additional_ratings)
+    cur.execute(create_additional_ratings)
+
 
     conn.commit()
     conn.close()
@@ -198,7 +223,7 @@ def insert_stuff_movies():
 
     for film in movie_dict:
 
-        insertion = (None, film,  movie_dict[film]['ranking'], movie_dict[film]['year_published'], movie_dict[film]['average_rating'], movie_dict[film]['content_rating'])
+        insertion = (None, film, movie_dict[film]['ranking'], movie_dict[film]['year_published'], movie_dict[film]['average_rating'], movie_dict[film]['content_rating'])
         statement = 'INSERT INTO "Movies" '
         statement += 'VALUES (?, ?, ?, ?, ?, ?)'
         cur.execute(statement, insertion)
@@ -237,24 +262,45 @@ def create_director():
     conn.close()  
 
 def create_additional_movies():
-    file_contents = open('IMDB_movies.csv', 'r')
+    file_contents = open('IMDb_movies.csv', 'r')
     csv_reader = csv.reader(file_contents)
+    conn = sqlite3.connect(DBNAME)
+    #conn.text_factory = str 
+    curr = conn.cursor()
+
+
+    for row in csv_reader:
+
+        insertion = (None, row[0], row[1], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16])
+        statement = 'INSERT INTO "Additional_Movies"'
+        statement += 'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+        curr.execute(statement, insertion) 
+
+    conn.commit()
+    conn.close()
+
+
+def create_additional_ratings():
+    file_contents = open('IMDb_ratings.csv', 'r')
+    csv_reader = csv.reader(file_contents)
+
     conn = sqlite3.connect(DBNAME) 
-    conn.text_factory = str 
+    #conn.text_factory = str 
     curr = conn.cursor()
 
     for row in csv_reader:
 
-        insertion = (None, row[1], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16])
-        statement = 'INSERT INTO "Additional_Movies"'
-        statement += 'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+        insertion = (None, row[0], row[2], row[3], row[4], row[32], row[42])
+        statement = 'INSERT INTO "Additional_Movies_Ratings"'
+        statement += 'VALUES (?, ?, ?, ?, ?, ?, ?)'
         curr.execute(statement, insertion) 
 
     conn.commit()
-    conn.close()            
+    conn.close()
+
 	
 
-#Flask App Attempt
+#Interactive Portion
 
 def plot_year_count():
     conn = sqlite3.connect(DBNAME)
@@ -369,26 +415,144 @@ def plot_duration():
     fig = go.Figure([go.Bar(x=duration, y=count, text=count, textposition='auto')])  
     fig.show()
 
+def table_movies():
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
+
+    look_up = '''
+
+        SELECT Ranking, Title
+        FROM Movies; 
+
+
+    '''
+
+    query = cur.execute(look_up)
+    query = list(cur.fetchall())
+    conn.commit()
+    conn.close()
+
+    ranking = []
+    title = []
+
+    for i in query:
+        ranking.append(i[0])
+        title.append(i[1])
+
+    fig = go.Figure(data = [go.Table(header = dict(values=['Movie Title', 'Ranking']), cells =dict(values=[[x for x in title], [x for x in ranking]])) 
+    ])
+    fig.show()
+
+def plot_rating_content():
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
+
+    look_up = '''
+
+        SELECT AverageRating, Ranking
+        FROM Movies;
+
+
+    '''
+
+    query = cur.execute(look_up)
+    query = list(cur.fetchall())
+    conn.commit()
+    conn.close()
+
+    AverageRating = []
+    Rank = []
+
+    for i in query:
+        AverageRating.append(i[0])
+        Rank.append(i[1])
+
+    fig = go.Figure([go.Scatter(x=Rank, y=AverageRating)])  
+    fig.show()
+
+def director_count():
+    conn = sqlite3.connect(DBNAME) 
+    curr = conn.cursor()
+
+    look_up = '''
+
+        SELECT DirectorName, COUNT(*) as MovieNum
+        FROM Directors LEFT JOIN Movies ON Directors.MovieName = Movies.Title
+        GROUP BY DirectorName
+        HAVING MovieNum > 1
+        ORDER BY MovieNum DESC;
+
+    '''
+
+    query = curr.execute(look_up)
+    query = list(curr.fetchall())
+
+    director = []
+    count = []
+    for i in query:
+        director.append(i[0])
+        count.append(i[1])
+
+    fig = go.Figure([go.Bar(x=director, y=count, text = count, textposition = 'auto')])
+    fig.show()       
     
+
+def actor_count():
+    conn = sqlite3.connect(DBNAME) 
+    curr = conn.cursor()
+
+    look_up = '''
+
+    SELECT ActorName, COUNT(*) as MovieNum
+    FROM Stars LEFT JOIN Movies ON Stars.MovieName = Movies.Title
+    GROUP BY ActorName
+    HAVING MovieNum > 1
+    ORDER BY MovieNum DESC; 
+
+    '''
+
+    query = curr.execute(look_up)
+    query = list(curr.fetchall())
+
+    conn.commit()
+    conn.close()
+
+    actor = []
+    count = []
+    
+    for i in query:
+        actor.append(i[0])
+        count.append(i[1])
+
+    fig = go.Figure([go.Bar(x=actor, y=count, text=count, textposition = 'auto')])
+    fig.show()   
+
+
+
 if __name__ == '__main__':
     #app.run(debug = True)
     CACHE_DICT = open_cache()
     init_db()
-    insert_stuff_movies()
     create_star()
     create_director()
+    insert_stuff_movies()
     create_additional_movies()
+    create_additional_ratings()
 
     print('Welcome to the Movie Information Data House')
     print('')
     print('Here are your options for information')
     user1 = '''
-    Option 1: See a Graph of Number of Movies Per Year from an IMDb database (Enter 1) 
-    Option 2: See a Graph of the Number of Movies Per Duration from an IMDb database (Enter 2)
-    Option 3: See a Graph of Average Content from IMDb Top 250 Chart (Enter 3)
-    Option 4: See a Graph of Avergae Ratings from IMDB Top 250 Chart (Enter 4)
-    Option 5: Exit the Data House
-    Option 6: Bring up the menu again 
+    Option 1: See a table of the IMDb Top 250 Movies (Enter 1)
+    Option 2: See a Graph of Number of Movies Per Year from an IMDb database (Enter 2) 
+    Option 3: See a Graph of the Number of Movies Per Duration from an IMDb database (Enter 3)
+    Option 4: See a Graph of Average Content from IMDb Top 250 Chart (Enter 4)
+    Option 5: See a Graph of Avergae Ratings from IMDb Top 250 Chart (Enter 5)
+    Option 6: See a Graph of the Rank and Average Rating from the IMDb Top 250 Chart (Enter 6)
+    Option 7: See a Graph of the Top Count of Directors from the IMDb Top 250 Chart (Enter 7)
+    Option 8: See a Graph of the Top Count of Stars from the IMDb Top 250 Chart (Enter 8)  
+    Option 9: Exit the Data House (Enter 9)
+    Option 10: Bring up the menu again (Enter 10) 
     '''
 
     print(user1)
@@ -397,46 +561,47 @@ if __name__ == '__main__':
     while True:
         user_input = input('Please select a number: ')
         if user_input == '1':
+            print('Table of Top 250 Movies')
+            print('May take a while to appear in webbrowser')
+            table_movies()
+        elif user_input == '2':
             print('Ploting Number of Movies per Year')
             print('May take a while to appear in webbrowser')
-            plot_rating_count()
-        elif user_input == '2':
+            plot_year_count()
+        elif user_input == '3':
             print('Ploting Number of Movies per Duration')
             print('May take a while to appear in webbrowser')
             plot_duration()
-        elif user_input == '3':
+        elif user_input == '4':
             print('Ploting Average Content Rating from Top 250')
             print('May take a while to appear in webbrowser')
             plot_rating_count()
-        elif user_input == '4':
+        elif user_input == '5':
             print('Ploting Average Rating Score from Top 250')
             print('May take a while to appear in webbrowser')
             plot_average_count()
-        elif user_input == '5':
-            break
         elif user_input == '6':
+            print('Ploting Graph of the Rank versus Average Rating')
+            print('May take a while to appear in webbrowser')
+            plot_rating_content()
+        elif user_input == '7': 
+            print('Ploting Graph of Top Director Count')
+            print('May take a while to appear in webbrowser')
+            director_count()
+        elif user_input == '8':
+            print('Ploting Graph of Top Actor Count')
+            print('May take a while to appear in webbrowser')
+            actor_count()       
+        elif user_input == '9':
+            break
+        elif user_input == '10':
             print(user1)
         else:
-            print('[Error] Not a Valid Input, please select number 1-6')     
+            print('[Error] Not a Valid Input, please select number 1-10')     
 
     print('Now Exiting Application: Thank you for visiting the Movie Data House!')
 
 
-
-
-
-  
-
-    
-
-  
-		
-
-	
-		
-    
-
-#print(film_dict[movie_name]['movie_name'])    
 
 
 
